a12e752208252dee89ebcffdeb4e08a93bddd1aa
tracer11 <karolgurba22@gmail.com>
2018-03-06 21:15:30 -0600

    added more validations

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ var UIController = (function() {
       return {
         type: document.querySelector(DOMstrings.inputType).value, // either inc or exp
         description: document.querySelector(DOMstrings.inputDescription).value,
-        value: document.querySelector(DOMstrings.inputValue).value,  
+        value: parseFloat(document.querySelector(DOMstrings.inputValue).value)  
       };
     },
 
@@ -123,20 +123,28 @@ var controller = (function(budgetCtrl, UICtrl) {
   }
 
 
+  var updateBudget = function(){
+    // 1 calc the budget
+
+    // 2 return budget
+
+    // 3 display the budget on the UI
+  }
 
   var ctrlAddItem = function() {
     var input, newItem;
     // 1 get the filled input data
     input = UICtrl.getinput();
-    // 2 add the item to the budget controller
-    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
-    // 3 add the item to the UI
-    UICtrl.addListItem(newItem, input.type)
-    // 4 clear input fields
-    UICtrl.clearFields();
-    // 5 calc the budget
-    // 6 display the budget on the UI
-
+    if(input.description !== "" && !isNaN(input.value) && input.value > 0){
+      // 2 add the item to the budget controller
+      newItem = budgetCtrl.addItem(input.type, input.description, input.value);
+      // 3 add the item to the UI
+      UICtrl.addListItem(newItem, input.type);
+      // 4 clear input fields
+      UICtrl.clearFields();
+      // 5 calculate and update budget
+      updateBudget(); 
+    }
   };
 
   return {
